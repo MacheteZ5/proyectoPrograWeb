e64f1e3ff144a57f32af23f0f31b0da75e2ceced
MacheteZ5 <machetemontenegro@hotmail.com>
2023-02-12 15:31:12 -0600

    creación de la vista login junto al controlador del Proyecto. Además se realizaron modificaciones en el archivo Startup.cs para colocar por defecto que siempre inicie con la vista login

---
+++ Proyecto_Progra_Web/Proyecto_Progra_Web/Controllers/ProyectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Proyecto_Progra_Web.Models;
+using Proyecto_Progra_Web.Controllers;
+
+namespace Proyecto_Progra_Web.Controllers
+{
+    public class ProyectController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+}
